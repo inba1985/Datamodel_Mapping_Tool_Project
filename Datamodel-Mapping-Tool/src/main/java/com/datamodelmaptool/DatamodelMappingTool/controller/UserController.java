d4d04d3e41a386c01086e4f795519b406b7be6b4
inba <inbakalathy@hotmail.com>
2023-11-12 15:07:05 +0800

    added fetch request

--- Datamodel-Mapping-Tool/src/main/java/com/datamodelmaptool/DatamodelMappingTool/controller/UserController.java
+++ Datamodel-Mapping-Tool/src/main/java/com/datamodelmaptool/DatamodelMappingTool/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.datamodelmaptool.DatamodelMappingTool.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,12 @@ public class UserController {
 	
 	User newUser(@RequestBody User newUser) {
 		return userRepository.save(newUser);
+	}
+	
+	@GetMapping("/users")
+	List<User> getAllUsers(){
+	
+		return userRepository.findAll();
 	}
 	
 
