f5c11bf5f95855618907ea25cda718b0b1adecd6
inba <inbakalathy@hotmail.com>
2023-11-12 13:43:52 +0800

    added project in new folder

---
+++ Datamodel-Mapping-Tool/src/main/java/com/datamodelmaptool/DatamodelMappingTool/DatamodelMappingToolApplication.java
@@ -0,0 +1,13 @@
+package com.datamodelmaptool.DatamodelMappingTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamodelMappingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamodelMappingToolApplication.class, args);
+	}
+
+}
---
+++ Datamodel-Mapping-Tool/src/test/java/com/datamodelmaptool/DatamodelMappingTool/DatamodelMappingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datamodelmaptool.DatamodelMappingTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamodelMappingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
